407bdee666f926d58acff9523b2f8d26977491d7
Immolazione <federico.deered@gmail.com>
2022-03-02 15:29:27 +0100

    try to simplify the excercise

--- index.php
+++ index.php
@@ -13,22 +13,26 @@ class Movies {
     public $genre;
     public $year;
 
-    public function __construct($name, $genre, $year){
-        $this->name = $name;
-        $this->genre = $genre;
-        $this->year = $year;
-    }
+    // public function __construct($name, $genre, $year){
+    //     $this->name = $name;
+    //     $this->genre = $genre;
+    //     $this->year = $year;
+    // }
 
-};
+}
 
 // $movie1 = new Movies('Lord of the Ring', 'Adventure', '2001');
 // $movie1 = new Movies('Godzilla', 'Sci-Fi', '2014');
 
-// var_dump($movie1);
-    
+
 // echo 'Primo film: ' . $movie1;
 // echo 'Secondo film: ' . $movie2;
 
+$movie1 = new Movies();
+$movie1->name = 'Godzilla';
+
+var_dump($movie1);
+echo $movie1;
 
 ?>
 
