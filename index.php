d357c89a88398e52ef3d445a29f865dcdc4a287e
Immolazione <federico.deered@gmail.com>
2022-03-02 15:24:21 +0100

    struggle on show in page results

--- index.php
+++ index.php
@@ -13,18 +13,22 @@ class Movies {
     public $genre;
     public $year;
 
-    public function __construct($name, $cover, $year){
+    public function __construct($name, $genre, $year){
         $this->name = $name;
-        $this->cover = $cover;
+        $this->genre = $genre;
         $this->year = $year;
     }
 
 };
 
-$movie1 = new Movies('Lord of the Ring', 'Adventure', '2001');
-$movie1 = new Movies('Godzilla', 'Sci-Fi', '2014');
+// $movie1 = new Movies('Lord of the Ring', 'Adventure', '2001');
+// $movie1 = new Movies('Godzilla', 'Sci-Fi', '2014');
+
+// var_dump($movie1);
     
-echo $movie1;
-echo $movie2;
+// echo 'Primo film: ' . $movie1;
+// echo 'Secondo film: ' . $movie2;
+
+
 ?>
 
